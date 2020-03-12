7d151e657d7edd2dd92db009621b71bed6f17b47
Brandon Kappel <KappelB@smscgc.org>
2020-03-12 16:15:51 -0500

    bump

--- src/app/effortless/on-floor/on-floor-slot/edit-seals/break-seal/break-seal.component.ts
+++ src/app/effortless/on-floor/on-floor-slot/edit-seals/break-seal/break-seal.component.ts
@@ -37,8 +37,8 @@ export class BreakSealComponent extends EffortlessComponentBase implements OnIni
   break() {
     this.gds.editSealPayload.BrokenSeals.push({ SealNumber: this.seal.SealNumber, BrokenReason: this.replacementReason });
     this.dialogRef.close()
-console.error(this.seal.SealNumber)
-console.error(this.replacementReason)
+    console.error(this.seal.SealNumber)
+    console.error(this.replacementReason)
   }
 
   cancelBreakSeal() {
