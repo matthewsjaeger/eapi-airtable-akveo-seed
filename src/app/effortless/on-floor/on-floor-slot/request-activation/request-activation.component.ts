ab17add3d521bc3d8bb16b9d20c4f392e737e2cf
Brandon Kappel <KappelB@smscgc.org>
2020-04-02 12:06:35 -0500

    updated request activation

--- src/app/effortless/on-floor/on-floor-slot/request-activation/request-activation.component.ts
+++ src/app/effortless/on-floor/on-floor-slot/request-activation/request-activation.component.ts
@@ -13,23 +13,25 @@ import { NbMenuService, NbListComponent } from '@nebular/theme';
 export class RequestActivationComponent extends EffortlessComponentBase implements OnInit {
 
   checklist: any = {
-    MachineNumber: '', LicenseNumber: '', SerialNumber: '', MetersRecorded: '', ZeroMeters: '',
-    SlotLine: '', MasterChip: '', NewAddress: '', GameName: '', ChipOptions: '', MachineOptions: '',
-    RealTime: '', SiteId: '', Cl: '', Glass: '', MachinePlaques: '', ButtonLegends: '', Awards: '',
-    CommunicationType: '', Inserts: '', HandPay: '', Candle: '', TopAward: '', DemoTicket: '', CoinCredit: '',
-    ConnectSlot: '', CoinTest: '', BillTest: '', BillDoor: '', DoorDrop: '', MainDoor: '', AuxDoor: '', GlassMatch: '',
-    Reboot: '', TITOTest: '', TouchScreen:'', CardReader: '', ProgressiveIncrement: '', IncrementValue:'', 
-    BaseValue: '', LogicCage: '', SignedOff: ''
+    ScheduledChanges: '' 
+    // LicenseNumber: '', SerialNumber: '', MetersRecorded: '', ZeroMeters: '',
+    // SlotLine: '', MasterChip: '', NewAddress: '', GameName: '', ChipOptions: '', MachineOptions: '',
+    // RealTime: '', SiteId: '', Cl: '', Glass: '', MachinePlaques: '', ButtonLegends: '', Awards: '',
+    // CommunicationType: '', Inserts: '', HandPay: '', Candle: '', TopAward: '', DemoTicket: '', CoinCredit: '',
+    // ConnectSlot: '', CoinTest: '', BillTest: '', BillDoor: '', DoorDrop: '', MainDoor: '', AuxDoor: '', GlassMatch: '',
+    // Reboot: '', TITOTest: '', TouchScreen:'', CardReader: '', ProgressiveIncrement: '', IncrementValue:'', 
+    // BaseValue: '', LogicCage: '', SignedOff: ''
     };
 
   tempChecklist: any = {
-    MachineNumber: {}, LicenseNumber: {}, SerialNumber: {}, MetersRecorded: {}, ZeroMeters: {},
-    SlotLine: {}, MasterChip: {}, NewAddress: {}, GameName: {}, ChipOptions: {}, MachineOptions: {},
-    RealTime: {}, SiteId: {}, Cl: {}, Glass: {}, MachinePlaques: {}, ButtonLegends: {}, Awards:{},
-    CommunicationType: {}, Inserts: {}, HandPay: {}, Candle: {}, TopAward: {}, DemoTicket: {}, CoinCredit: {},
-    ConnectSlot: {}, CoinTest: {}, BillTest: {}, BillDoor: {}, DoorDrop: {}, MainDoor: {}, AuxDoor: {}, GlassMatch: {},
-    Reboot: {}, TITOTest: {}, TouchScreen: {}, CardReader: {}, ProgressiveIncrement: {}, IncrementValue:{}, 
-    BaseValue: {}, LogicCage: {}, SignedOff: {},
+    ScheduledChanges: {}, 
+    // LicenseNumber: {}, SerialNumber: {}, MetersRecorded: {}, ZeroMeters: {},
+    // SlotLine: {}, MasterChip: {}, NewAddress: {}, GameName: {}, ChipOptions: {}, MachineOptions: {},
+    // RealTime: {}, SiteId: {}, Cl: {}, Glass: {}, MachinePlaques: {}, ButtonLegends: {}, Awards:{},
+    // CommunicationType: {}, Inserts: {}, HandPay: {}, Candle: {}, TopAward: {}, DemoTicket: {}, CoinCredit: {},
+    // ConnectSlot: {}, CoinTest: {}, BillTest: {}, BillDoor: {}, DoorDrop: {}, MainDoor: {}, AuxDoor: {}, GlassMatch: {},
+    // Reboot: {}, TITOTest: {}, TouchScreen: {}, CardReader: {}, ProgressiveIncrement: {}, IncrementValue:{}, 
+    // BaseValue: {}, LogicCage: {}, SignedOff: {},
   };
 
   checklistMetadata: any = {};
@@ -49,50 +51,50 @@ export class RequestActivationComponent extends EffortlessComponentBase implemen
 
   updatePercentComplete = function () {
     this.checklistMetadata.PercentComplete = 16;
-    if (this.checklist.MachineNumber) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.LicenseNumber) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.SerialNumber) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.MetersRecorded) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.ZeroMeters) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.SlotLine) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.MasterChip) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.NewAddress) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.GameName) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.ChipOptions) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.MachineOptions) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.RealTime) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.SiteId) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.Cl) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.Glass) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.MachinePlaques) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.ButtonLegends) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.Awards) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.CommunicationType) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.Inserts) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.HandPay) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.Candle) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.TopAward) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.DemoTicket) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.CoinCredit) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.ConnectSlot) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.CoinTest) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.BillTest) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.BillDoor) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.DoorDrop) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.MainDoor) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.AuxDoor) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.GlassMatch) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.Reboot) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.TITOTest) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.TouchScreen) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.CardReader) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.ProgressiveIncrement) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.IncrementValue) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.BaseValue) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.LogicCage) this.checklistMetadata.PercentComplete += 2;
-    if (this.checklist.SignedOff) this.checklistMetadata.PercentComplete += 2;
+    if (this.checklist.ScheduledChanges) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.LicenseNumber) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.SerialNumber) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.MetersRecorded) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.ZeroMeters) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.SlotLine) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.MasterChip) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.NewAddress) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.GameName) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.ChipOptions) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.MachineOptions) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.RealTime) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.SiteId) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.Cl) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.Glass) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.MachinePlaques) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.ButtonLegends) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.Awards) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.CommunicationType) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.Inserts) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.HandPay) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.Candle) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.TopAward) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.DemoTicket) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.CoinCredit) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.ConnectSlot) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.CoinTest) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.BillTest) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.BillDoor) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.DoorDrop) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.MainDoor) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.AuxDoor) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.GlassMatch) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.Reboot) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.TITOTest) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.TouchScreen) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.CardReader) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.ProgressiveIncrement) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.IncrementValue) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.BaseValue) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.LogicCage) this.checklistMetadata.PercentComplete += 2;
+    // if (this.checklist.SignedOff) this.checklistMetadata.PercentComplete += 2;
     this.checklistMetadata.Status = (this.checklistMetadata.PercentComplete == 100) ? 4 : 1;
-    this.checklistMetadata.ComplianceStatus = (!this.checklist.Verification || !this.checklist.MediaSignature) ? 1 : 
+    this.checklistMetadata.ComplianceStatus = (this.checklist.ScheduledChanges == 'Fail') ? 1 : 
     (this.checklistMetadata.PercentComplete == 100) ? 0 : 2;
   };
 
